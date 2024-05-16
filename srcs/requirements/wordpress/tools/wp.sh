80940b1a48e5fae683713d1f40d3a3fef62bde02
Gabriela <krustagabriela@gmail.com>
2024-05-16 17:54:49 +0200

    WordPress lacks conf file

---
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+WP_DIR=/var/www/html/wordpress
+
+if [ ! -d "$WP_DIR" ]
+then
+	mkdir -p "$WP_DIR"
+fi
+
+if [ ! -f wp-config.php ]
+echo "Setting up WordPress"
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
+chmod +x wp-cli.phar 
+mv wp-cli.phar /usr/local/bin/wp
+
+cd "$WP_DIR"
+
+wp core download --allow-root
+
+mv wp-config-sample.php wp-config.php
+
+sed -i "s/database/$MYSQL_DATABASE/" wp-config.php
+sed -i "s/database_user/$WORDPRESS_USER/" wp-confi
+sed -i "s/passwod/$WORDPRESS_USER_PASSWORD/" wp-config.php
+sed -i "s/localhost/mariadb/" wp-config.php 
+
+wp core install --url=$WORDPRESS_URL --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --skip-email --allow-root
+wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --role=author --user_pass=$WORDPRESS_USER_PASSWORD --allow-root
+
+sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+fi
+
+echo "WordPress started"
+exec /usr/sbin/php7.3-fpm -F
