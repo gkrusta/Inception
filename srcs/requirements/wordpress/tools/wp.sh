06b0f8a61af76c5dc023d7aee882f6440c887db0
Gabriela <krustagabriela@gmail.com>
2024-05-19 22:27:26 +0200

    Error establishing a database connection.

--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -9,8 +9,8 @@ then
 fi
 
 chown -R www-data:www-data "$WP_DIR"
+chmod 755 "$WP_DIR"
 
-if [ ! -f wp-config.php ]
 echo "Setting up WordPress"
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
 chmod +x wp-cli.phar 
@@ -18,20 +18,18 @@ mv wp-cli.phar /usr/local/bin/wp
 
 cd "$WP_DIR"
 
-wp core download --allow-root
+sed -i "s/database_name_here/$MYSQL_DATABASE/" wp-config-sample.php
+sed -i "s/username_here/$MYSQL_USER/" wp-config-sample.php
+sed -i "s/password_here/$MYSQL_USER_PASSWORD/" wp-config-sample.php
+sed -i "s/localhost/mariadb:3306/" wp-config-sample.php
 
-mv wp-config-sample.php wp-config.php
-
-sed -i "s/database/$MYSQL_DATABASE/" wp-config.php
-sed -i "s/database_user/$WORDPRESS_USER/" wp-confi
-sed -i "s/passwod/$WORDPRESS_USER_PASSWORD/" wp-config.php
-sed -i "s/localhost/mariadb/" wp-config.php 
+cp wp-config-sample.php wp-config.php
 
 wp core install --url=$WORDPRESS_URL --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --skip-email --allow-root
 wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --role=author --user_pass=$WORDPRESS_USER_PASSWORD --allow-root
 
-sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
-fi
+sed -i 's|listen = /run/php/php7.4-fpm.sock|listen = 9000|' /etc/php/7.4/fpm/pool.d/www.conf
 
-echo "WordPress started"
-exec /usr/sbin/php7.3-fpm -F
+echo "Starting PHP-FPM"
+sleep 1
+/usr/sbin/php-fpm7.4 -F
