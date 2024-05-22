358abc8358050a16ffa85df98d50b2295858c97a
gkrusta <krustagabriela@gmail.com>
2024-05-22 20:31:13 +0200

    updated loc in nginx

--- srcs/requirements/nginx/tools/nginx.sh
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+openssl req -x509 -nodes -days 365 -newkey rsa:1024 \
 -keyout /etc/ssl/private/nginx-selfsigned.key \
--out c \
--subj "/C=Malaga/L=Malaga/O=42/OU=student/CN=gkrusta"
-
+-out /etc/ssl/certs/nginx-selfsigned.crt \
+-subj "/C=MA/L=MA/O=42/OU=student/CN=gkrusta"
+tail -f
 nginx -g "daemon off;"
